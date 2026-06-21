17acb0e4317e7700902ab559825f195df483bab7
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2026-06-21 11:36:15 +0530

    just login page

--- src/Layout/AuthLaoyut/AuthLaout.jsx
+++ src/Layout/AuthLaoyut/AuthLaout.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Logo from "../../Components/Logo/Logo"
-
+import { Outlet } from 'react-router';
+import g from "../../assets/png/TeamsDwon/authImage.png"
 
 
 
 const AuthLaout = () => {
     return (
-        <div className='p-10 mx-auto'>
+        <div className='max-w-7xl mx-auto'>
             <div className="flex">
                  <Logo></Logo> 
                  <p className='my-auto font-medium text-2xl'>Zapshfit</p> 
-
-                
-
+            </div>
+            <div className="flex my-20 ">
+                <div className="flex-1">
+                    <Outlet></Outlet>
+                </div>
+                <div className='flex-1'>
+                    <img src={g} alt="" />
+                </div>
             </div>
             
             
