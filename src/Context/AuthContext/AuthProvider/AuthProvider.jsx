629226872846e1cb1762253711d08e5af28dfa8c
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2026-06-22 22:35:27 +0530

    user stete manage

--- src/Context/AuthContext/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider/AuthProvider.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Authcontext } from '../AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from '../../../../FireBase/Firebase.config';
 
 const AuthProvider = ({children}) => {
+const [user , setUser] =useState()
+const [loading,setLoading ] = useState(true)
 
+const googleProvider = new GoogleAuthProvider()
     const registerUser =(email,password)=> {
+        setLoading(true)
 return createUserWithEmailAndPassword(auth,email,password)
     }
     const signInuser = (email,password)=>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth,email,password)
     }
+    const  siginWithGoogle =()=>{
+        setLoading(true)
+        return signInWithPopup(auth,googleProvider)
+    }
+
+   useEffect(()=>{
+    
+   },[])
 
     const authinfo = {
 registerUser,
-signInuser
+signInuser,
+siginWithGoogle,
+user,
+loading
+
     }
     return (
         <Authcontext value={authinfo}>
--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { NavLink } from 'react-router';
 import useAuth from '../../../Hooks/useAuth/useAuth';
+import Google from '../SocialLogin/Google';
 
 const Register = () => {
 const {registerUser,} =useAuth()
@@ -23,8 +24,8 @@ const {registerUser,} =useAuth()
         <div>
             
             <div className="card-body">
-                             <h2 className='text-4xl font-bold my-2'> Welcome Back</h2>
-                        <p className=''>Login with ZapShift</p>
+                             <h2 className='text-4xl font-bold my-2'> Create Account </h2>
+                        <p className=''>Register with ZapShift</p>
                     <form onSubmit={handleSubmit(submitHeandel)} >
                         <fieldset className="fieldset">
                       <label className="label  text-[17px]">Name</label>
@@ -45,16 +46,13 @@ const {registerUser,} =useAuth()
                             <p className='text-red-500'>Password is required!! </p>
                         )
                       }
-                      <div><a className="link link-hover">Forgot password?</a></div>
-                      <NavLink to="/login"><p> back to Login </p></NavLink>
+                      
+                      <NavLink to="/login"><p className='text-blue-600 hover:underline'> back to Login </p></NavLink>
                      
                       
-                      <button className="btn btn-neutral mt-4 lg:w-[380px] rounded-full">Login</button>
+                      <button className="btn btn-neutral mt-4 lg:w-[380px] rounded-full">Register</button>
                       {/* google btn */}
-                      <button className="btn lg:w-[380px] mt-5 rounded-full bg-[#C2C2C2] text-black border-[#e5e5e5]">
-              <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-              Login with Google
-            </button>
+                      <Google></Google>
                     </fieldset>
                     </form>
                   </div>
