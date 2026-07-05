56da05f3c6f12effb6bd18ac7e4fde98b0b53a47
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2026-07-05 10:02:17 +0530

    Dainamic dashbord

--- src/Pages/Sheard/Navbar/Navbar.jsx
+++ src/Pages/Sheard/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <NavLink to="aboutUs" ><li>About Us</li></NavLink>
         <NavLink to="bargainnig" ><li>Pricing</li></NavLink>
         <NavLink to="send_a_parcel"><li>Send Parcel</li> </NavLink>
-        <NavLink to="bearider" ><li>Be a Rider</li></NavLink>
+        {/* <NavLink to="bearider" ><li>Be a Rider</li></NavLink> */}
 
         {
           user && <> 
