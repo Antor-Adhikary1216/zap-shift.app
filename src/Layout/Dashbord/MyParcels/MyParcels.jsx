453150b1a1553c77a4e801225aaf53a26155cd9f
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2026-07-05 16:44:26 +0530

    Delete function

--- src/Layout/Dashbord/MyParcels/MyParcels.jsx
+++ src/Layout/Dashbord/MyParcels/MyParcels.jsx
@@ -2,7 +2,10 @@ import { useQueries, useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../Hooks/useAuth/useAuth";
 import UseaxiosSecure from "../../../Hooks/useAxios/useaxiosSecure";
-import { FaRupeeSign } from "react-icons/fa";
+import { FaEdit, FaRupeeSign } from "react-icons/fa";
+import { RiEdit2Fill } from "react-icons/ri";
+import { MdDeleteForever } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
 
 const MyParcels = () => {
   const { user } = useAuth();
@@ -16,8 +19,19 @@ const MyParcels = () => {
     },
   });
 
+//    Handeling ==>
+    const parcelhandelDelete = (id)=>{
+        console.log(id , "delete items")
+
+
+    }
+
+
+
+
+
   return (
-    <div className="px-3">
+    <div className="px-2">
       <h2 className="text-5xl my-10 font-semibold ">
         {" "}
         All my parcels : {parcels.length}
@@ -31,19 +45,46 @@ const MyParcels = () => {
               <th>Parcel Name</th>
               <th>Parcel Type</th>
               <th>Payment Status</th>
-              <th>Actions</th>
-              <th className="flex items-center gap-2">Parcel cost <span><FaRupeeSign /></span></th>
+              <th className="flex items-center gap-2">
+                Parcel cost{" "}
+                <span>
+                  <FaRupeeSign />
+                </span>
+              </th>
+               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {parcels.map((parcel, index) => (
               <tr key={parcel._id}>
                 <th>{index + 1}</th>
-                <td>{parcel.parcelName}</td>
-                <td>{parcel.parceltype}</td>
-                <td>Panding</td>
-                  <td>Delete </td>
-                <td className="flex items-center gap-2">{parcel.cost} <span><FaRupeeSign /></span></td> 
+                <td className="text-xl">{parcel.parcelName}</td>
+                <td className="text-xl">{parcel.parceltype}</td>
+                <td className="text-xl">Panding</td>
+                {/* cost */}
+                <td className="flex items-center gap-2 text-xl">
+                  {parcel.cost}
+                  <span>
+                    <FaRupeeSign />
+                  </span>
+                </td>
+
+                {/* actions */}
+                <td className="px-2 ">
+                  <button className="btn btn-square mx-2 items-center border-none bg-none shadow-none text-xl">
+                    <FaEdit />
+                  </button>
+
+                  <button className="btn btn-square mx-2 items-center border-none bg-none shadow-none text-xl">
+                  <IoIosSearch />
+                  </button>
+
+                  <button onClick={()=>parcelhandelDelete (parcel._id) } className="btn btn-square mx-2 items-center border-none bg-none shadow-none text-xl">
+                  <MdDeleteForever />
+                  </button>
+                  
+                   
+                </td>
               </tr>
             ))}
           </tbody>
